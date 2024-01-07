d8301ad9827eae6e5752e6e0ef88eb48b597109d
Davidhernandezn <d4vidhernandezn@gmail.com>
2024-01-06 18:44:10 -0600

    Se crearon los catalogos y se importaron

---
+++ catalogs/catalogs.js
@@ -0,0 +1,45 @@
+let status_list = [{
+    text: 'Seleccione una opción',
+    value: 'option'
+},
+{
+    text: 'alive',
+    value: 'alive'
+},
+{
+    text: 'dead',
+    value: 'dead'
+},
+{
+    text: 'unknown',
+    value: 'unknown'
+    }
+]
+
+let gender_list = [{
+    text: 'Seleccione una opción',
+    value: 'option'
+},
+{
+    text: 'famele',
+    value: 'famele'
+},
+{
+    text: 'male',
+    value: 'male'
+},
+{
+    text: 'genderless',
+    value: 'genderless'
+    },
+{
+    text: 'unknown',
+    value: 'unknown'
+    }
+]
+
+//EXPORTAMOS VARIABLES
+export{
+    status_list,
+    gender_list
+}
--- src/buscador-component.js
+++ src/buscador-component.js
@@ -1,6 +1,8 @@
 import { LitElement, html, css } from 'lit-element';
 import { buscadorStyles } from '../css/buscador-styles';
 import { generalStyles } from '../css/general-styles';
+import { gender_list, status_list } from '../catalogs/catalogs';//IMPORTAMOS CATALOGOS
+
 export class BuscadorComponent extends LitElement{
     static get styles(){
     //IMPORTAMOS MODULOS CSS
@@ -13,12 +15,21 @@ export class BuscadorComponent extends LitElement{
 // DEFINIR PROPIEDADES
     static get properties(){
     return{
+        //IMPOTRAMOS EN LAS PROPIEDADES LOS CATALOGOS
+        status_list: {type: Array},
+        gender_list: {type: Array}
     }  
 }
 
 // PARA INICIALIZAR PROPIEDADES
 constructor(){
     super();
+    //ESTAS PROPIEDADES TOMAN SU VALOR DEL ARRAY IMPORTADO
+    this.status_list = status_list;
+    this.gender_list = gender_list;
+
+    console.log(status_list);
+    console.log(gender_list);
   }
 
 // render para llamar al template
