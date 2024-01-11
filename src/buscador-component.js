9f6922cfff21f9be2f508790f6a31fe1f737a5ef
Davidhernandezn <d4vidhernandezn@gmail.com>
2024-01-10 19:29:52 -0600

    Se agrego limpieza de HTML y Componentes

--- src/buscador-component.js
+++ src/buscador-component.js
@@ -179,7 +179,7 @@ export class BuscadorComponent extends LitElement {
                 <span class="error-msg" id="error"></span>
                 </div-input>
 
-                <button-primary class="button-primary buttonForm">Limpiar</button-primary>
+                <button-primary class="button-primary buttonForm" @click="${this.cleanParams}" >Limpiar</button-primary>
                 <svg class="ic-search" @click="${this.searchParams}" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
   <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
 </svg>
@@ -240,6 +240,7 @@ export class BuscadorComponent extends LitElement {
         .then(response => response.json()) // Cambiado "respuesta" a "response"
         .then(respuesta => {
             console.log(respuesta.results);
+            this.clean();//LIMPIAR CADA VEZ QUE HAY CONSULTA
 
             //ACCEDER A LA INFO, 
             respuesta.results.forEach(element =>{
@@ -261,6 +262,22 @@ export class BuscadorComponent extends LitElement {
             })
         })
     
+    }
+
+//LIMPIAR HTML
+//CUANDO SE LLAMA EL API
+    clean(){
+        this.shadowRoot.querySelector("#buscador").innerHTML = "";
+    }
+
+//LIMPIAR INPUT
+    cleanParams(){
+        this.shadowRoot.querySelector("#name").value = '';
+        this.shadowRoot.querySelector("#status").value = '';
+        this.shadowRoot.querySelector("#species").value = '';
+        this.shadowRoot.querySelector("#type").value = '';
+        this.shadowRoot.querySelector("#gender").value = '';
+        this.shadowRoot.querySelector("#page").value = '';
     }
 }
 
